96b84da9a2cdfaf5c5c6fc00e51d0372dbb33bf3
Prabisha01 <prabishakhadka2000@gmail.com>
2025-09-04 11:53:31 +0545

    udpated

---
+++ Week 1/prabisha_week1/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Student st = new Student();
+        System.out.println(st.getAge());
+        
+        Student st1 = new Student("js", 12);
+        System.out.println(st1.getAge());
+
+    }
+}
